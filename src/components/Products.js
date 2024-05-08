cf14c6ccaa71cf01a6215fbe436e3b8ab8b43996
robemike <robemike1135@gmail.com>
2024-05-08 15:56:24 +0300

    Creates component files

---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Products.css";
+import ProductsList from './ProductsList';
+
+function Products() {
+  return (
+    <div>
+      <ProductsList />
+    </div>
+  )
+}
+
+export default Products
---
+++ src/components/ProductsList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import "./ProductsList.css"
+
+
+function ProductsList() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/products')
+       .then(res => res.json())
+       .then(data => setProducts(data))
+    }, [])
+
+  return (
+    <div>
+      {products.map(product => (
+        <div className='card' key={product.id}>
+            <img src={product.image} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+          <Link to={`/products/${product.id}`}>{product.name}</Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ProductsList
