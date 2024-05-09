1d9f6aada8d01145250c9ec56783ff7b6f1fe376
robemike <robemike1135@gmail.com>
2024-05-09 16:36:04 +0300

    checkout

--- src/components/ProductInformation.js
+++ src/components/ProductInformation.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 function ProductInformation() {
     const { id } = useParams();
