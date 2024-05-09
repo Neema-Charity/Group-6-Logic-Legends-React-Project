49f54a32d892218f22d02a9ed1ceb98ea5678b81
robemike <robemike1135@gmail.com>
2024-05-09 10:16:52 +0300

    switch

--- src/components/ProductInformation.js
+++ src/components/ProductInformation.js
@@ -3,23 +3,18 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 
 function ProductInformation() {
-    const { id, category } = useParams();
-    const [product, setProduct] = useState(null);
-    const [relatedProducts, setRelatedProducts] = useState([]);
+
+    const { id } = useParams();
+    const [product, setProducts] = useState([])
 
     useEffect(() => {
         fetch(`http://localhost:3000/products/${id}`)
-            .then(res => res.json())
-            .then(data => setProduct(data));
-
-        // Fetch related products based on category
-        fetch(`http://localhost:3000/products?category=${category}&_limit=4`)
-            .then(res => res.json())
-            .then(data => setRelatedProducts(data));
-    }, [id, category]);
+           .then(res => res.json())
+           .then(data => setProducts(data))
+    }, [id])
 
     if (!product) {
-        return <h1>Loading...</h1>;
+        return <h1>Loading...</h1>
     }
 
     return (
@@ -28,21 +23,9 @@ function ProductInformation() {
                 <img src={product.image} alt={product.name} />
                 <strong>{product.description}</strong>
                 <p>${product.price}</p>
-                <Link to='/'>Back to Products</Link>
-            </div>
-
-            <h2>Related Products</h2>
-            <div className='related-products-container'>
-                {relatedProducts.map(relatedProduct => (
-                    <div key={relatedProduct.id}>
-                        <img src={relatedProduct.image} alt={relatedProduct.name} />
-                        <strong>{relatedProduct.name}</strong>
-                        <p>${relatedProduct.price}</p>
-                    </div>
-                ))}
             </div>
         </div>
-    );
+    )
 }
 
-export default ProductInformation;
+export default ProductInformation
