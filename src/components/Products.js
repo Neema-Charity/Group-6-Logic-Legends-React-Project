a23e4ca9b60a7b5aec20934759114f23d4e931ad
robemike <robemike1135@gmail.com>
2024-05-09 10:12:28 +0300

    saves changes to remote repo

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Products.css";
 import ProductsList from './ProductsList';
+import NewProductForm from './NewProductForm';
 
 function Products() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
+  const handleAddProduct = (newProduct) => {
+
+    setProducts([...products, newProduct]);
+
+    fetch('http://localhost:3000/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newProduct)
+    })
+      .then(res => res.json())
+      .then(data => console.log('New Product added', data))
+      .catch(error => console.error('Error in adding new Product', error))
+
+  }
+
   return (
     <div>
-      <ProductsList />
+      <NewProductForm onAddProduct={handleAddProduct} />
+      <ProductsList products={products} />
     </div>
   )
 }
