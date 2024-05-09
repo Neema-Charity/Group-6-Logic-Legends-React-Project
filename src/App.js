0269cb90e27ec2197ff32e3055ba5c94278c8b91
Neema-Charity <ncharityyy@gmail.com>
2024-05-10 01:46:39 +0300

    fix product list

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         </Routes>
       </div>
       {/* Conditionally render the SelectedProduct component */}
-      {selectedProduct && <SelectedProduc product={selectedProduct}/>}
+      {selectedProduct && <SelectedProduct product={selectedProduct}/>}
       <Footer />
     </Router>
   );
