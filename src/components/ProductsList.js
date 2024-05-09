91d87a42925978735a4c0ef347e7e1d5dbe9a324
robemike <robemike1135@gmail.com>
2024-05-10 00:05:55 +0300

    project deployment

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -20,7 +20,7 @@ function ProductsList() {
       <div>
         <h1 id="shoppers" className="bg-success p-2 text-white">SHOPPERS</h1>
       </div>
-      <div><Link style={{ color: '#198754' }} className='btn btn-primary' to='/admins'>Admins Panel</Link></div>
+      <div><Link style={{ color: '#198754' }} className='admin-btn btn-primary' to='/admins'>Admins Panel</Link></div>
       <div className='products-list-container'>
         {products.map(product => (
           <div className="bg-success p-2 text-dark bg-opacity-25" id="card" key={product.id}>
