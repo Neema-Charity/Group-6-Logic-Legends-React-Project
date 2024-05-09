648fa0d0d3bc49d071db5694b6e6c35948733239
robemike <robemike1135@gmail.com>
2024-05-09 16:38:45 +0300

    updates application

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -8,15 +8,15 @@ function ProductsList({ products }) {
     <div>
       <div>
         <h1 id="shoppers" className="bg-success p-2 text-white">SHOPPERS</h1>
+        <Cart />
       </div>
-      <Cart />
-      <div className='products-list-container'> 
+      <div className='products-list-container'>
         {products.map(product => (
           <div className="bg-success p-2 text-dark bg-opacity-25" id="card" key={product.id}>
             <img className='card-img-top rounded-circle product-image' src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p>${product.price}</p>
-            <Link style={{color: '#198754'}} to={`/products/${product.id}`}>INFO TO ORDER!</Link>
+            <Link style={{ color: '#198754' }} to={`/products/${product.id}`}>INFO TO ORDER!</Link>
           </div>
         ))}
       </div>
